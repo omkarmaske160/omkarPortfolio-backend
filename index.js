10f3aca911b9d05327be7568d362f7794f120dc8
omkar mhaske <135412726+omkarmaske160@users.noreply.github.com>
2024-06-17 10:07:56 +0530

    disk

--- index.js
+++ index.js
@@ -6,30 +6,35 @@ const path = require("path")
 
 
 const app = express()
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }))
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.json())
+// app.use(express.static("assets/projectImg"))
 app.use(express.static(path.join(__dirname, "assets/projectImg")))
-app.use(express.static("assets/projectImg"))
-// app.use(express.static(path.join(__dirname, "dist")))
+app.use(express.static(path.join(__dirname, "dist")))
 
 mongoose.connect(process.env.MONGO_URL)
 
-// app.use("*", (req, res) => {
-//     // res.sendFile(path.join(__dirname, "dist", "index.html"))
-// })
 
 // routes
 app.use("/api/v1/admin", require("./routes/protectedRoute"))
 
-app.use("*", (req, res) => {
-    res.status(400).json({
-        message: "response not found"
-    })
-})
+// app.use("*", (req, res) => {
+//     res.status(400).json({
+//         message: "response not found"
+//     })
+// })
 
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
 //mongo db connection 
 mongoose.connection.once("open", () => {
