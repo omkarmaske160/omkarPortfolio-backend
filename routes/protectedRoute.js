693f74fff5b8cb1fb397935c960e8f33470e69aa
omkarmaske160 <omkarmaske160@gmail.com>
2025-09-17 10:28:28 +0530

    changes done

--- routes/protectedRoute.js
+++ routes/protectedRoute.js
@@ -1,6 +1,6 @@
 const { addProject, getAllProject, deleteProject } = require("../controller/adminController")
 const { addFeedback } = require("../controller/emailController")
-const { protected } = require("../middleware/protected")
+const { protected } = require("../middleware/Protected")
 
 
 const protectedRoute = require("express").Router()
